affd7f64524d47c5344602c0a36cc28b9b82595c
simran5555 <simran.kaur@ayasya.com>
2024-01-25 16:40:35 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const parent = React.createElement('div', { id: 'parent' }, [
+  React.createElement('div', { id: 'child1', key:1 }, [
+    React.createElement('h1', {key:1}, "h1 tag"),
+    React.createElement('h2', {key:2}, "h2 tag")
+  ]),
+  React.createElement('div', { id: 'child2', key:2 }, [
+    React.createElement('h1', {key:1}, "h1 tag"),
+    React.createElement('h2', {key:2}, "h2 tag")
+  ])
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(parent)
