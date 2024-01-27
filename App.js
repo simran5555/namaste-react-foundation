34c83b1cb24778028825f03e0743ea46d0f99951
simran5555 <simran.kaur@ayasya.com>
2024-01-27 16:06:28 +0530

    using jsx to create react elements

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
 
-const parent = React.createElement('div', { id: 'parent' }, [
-  React.createElement('div', { id: 'child1', key:1 }, [
-    React.createElement('h1', {key:1}, "h1 tag"),
-    React.createElement('h2', {key:2}, "h2 tag")
-  ]),
-  React.createElement('div', { id: 'child2', key:2 }, [
-    React.createElement('h1', {key:1}, "h1 tag"),
-    React.createElement('h2', {key:2}, "h2 tag")
-  ])
-])
+//creating a react element using core react
+//react.createelement -> reactelement (js object) -> htmlelement(render)
+const heading = React.createElement("h1", {id:"heading1", key:"reactheading"}, "react core heading")
+console.log(heading)
+
+//creating react element using jsx
+//jsx code is transpiled into react.createelement by babel(from parcel)  creating a reactelement and then rendered into an htmlelement
+const jsxHeading = <h1 id="heading2" key="jsxheading">jsx heading</h1>
+console.log(jsxHeading)
+//console.log(heading) === console.log(jsxHeading) (same object)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(parent)
+root.render(jsxHeading)
--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 - "start" : "parcel index.html"
 - "build" : "parcel build index.html"
 //usage - npm start, npm run start and npm run build
+
+# babel //read documents
+- it is a js compiler or transpiler
+- create browser compatible js
+- parcel uses babel to transpile jsx code into react code so that it can be understood by js engines
+- not only jsx to react but it also converts es6 js to a js that older browsers understand
+- go see how babel works behind the scene
